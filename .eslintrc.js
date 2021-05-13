c3325902afc0bd6022ab54489d83b2edc7506fd0
Brendan Eggers <eggers.brendan@gmail.com>
2021-05-12 20:59:13 -0400

    fix: trying to fix again

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,17 @@
 module.exports = {
   extends: ['eslint-config-eggdev'],
+  rules: {
+    // conflicts with monorepos and workspaces
+    'import/no-extraneous-dependencies': 'off',
+    // unnecessary for packages rely on exporting
+    'import/no-unused-modules': 'off',
+  },
+  root: true,
+  settings: {
+    'import/resolver': {
+      node: {
+        moduleDirectory: ['node_modules', 'test'],
+      },
+    },
+  },
 };
